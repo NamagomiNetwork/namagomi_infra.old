908aee05d53a6b770c2e9e172e561880038d7ea8
nabr <100725601+nabr2730@users.noreply.github.com>
2022-06-05 10:51:18 +0900

    update(skript): 起動時に最新の設定ファイルをダウンロードするように

--- Minecraft-Server/Namagomi-mod/start.sh
+++ Minecraft-Server/Namagomi-mod/start.sh
@@ -2,4 +2,10 @@
 
 cd /home/mc-server/mc || exit
 # 各種configはgithubから引っ張る
+rm ops.json server.properties whitelist.json
+wget -q --show-progress --https-only --timestamping \
+  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/ops.json \
+  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/server.properties \
+  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/whitelist.json
+
 screen -UAmdS mc java -server -Xmx10G -Duser.timezone=JST -jar server.jar
