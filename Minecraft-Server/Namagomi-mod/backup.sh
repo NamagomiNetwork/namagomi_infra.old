03b8fc35227f17eecb3e64fcc62712d38620a8ef
nabr <100725601+nabr2730@users.noreply.github.com>
2022-09-06 20:52:10 +0900

    add: backup skript

--- Minecraft-Server/Namagomi-mod/backup.sh
+++ Minecraft-Server/Namagomi-mod/backup.sh
@@ -1,15 +1,54 @@
-#!bin/bash
+#!/bin/bash
 
-DATE=$(date "+%Y-%m-%d-%H-%M")
+DATE=$(/usr/bin/date "+%Y-%m-%d-%H-%M")
 
-# mc鯖を停止
-sudo systemctl stop mc
 
-if [ ! -d /home/mc-server/mc/backup ]
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+/usr/bin/screen -S mod01 -X stuff "say サーバーは10分後に再起動します\015"
+/usr/bin/screen -S mod01 -X stuff "say くらえ！\015"
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+
+/usr/bin/sleep 300
+
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+/usr/bin/screen -S mod01 -X stuff "say サーバーは5分後に再起動します\015"
+/usr/bin/screen -S mod01 -X stuff "say くらえ！\015"
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+
+/usr/bin/sleep 180
+
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+/usr/bin/screen -S mod01 -X stuff "say サーバーは2分後に再起動します\015"
+/usr/bin/screen -S mod01 -X stuff "say くらえ！\015"
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+
+/usr/bin/sllep 60
+
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+/usr/bin/screen -S mod01 -X stuff "say サーバーは1分後に再起動します\015"
+/usr/bin/screen -S mod01 -X stuff "say くらえ！\015"
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+
+/usr/bin/sleep 30
+
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+/usr/bin/screen -S mod01 -X stuff "say サーバーは30秒後に再起動します\015"
+/usr/bin/screen -S mod01 -X stuff "say くらえ！\015"
+/usr/bin/screen -S mod01 -X stuff "say =======================================\015"
+
+/usr/bin/sleep 20
+
+/usr/bin/systemctl stop mod
+if [ ! -d /home/mcserver/minecraft/backup ]
 then
-    mkdir /home/mc-server/mc/backup
+    /usr/bin/mkdir /home/mcserver/minecraft/backup
+    /usr/bin/echo "ディレクトリが存在しなかったため作成しました"
+    /usr/bin/sleep 3
 fi
 
-tar czfP /home/mc-server/mc/backup/mc-backup-${data}.tar.gz /home/mc-server/mc
+/usr/bin/tar -zcvf /home/mcserver/minecraft/backup/mc-backup-"${DATE}".tar.gz /home/mcserver/minecraft/mod
 
-sudo systemctl start mc
+
+/usr/bin/echo "バックアップ成功でやんす"
+
+/usr/bin/systemctl start mod
