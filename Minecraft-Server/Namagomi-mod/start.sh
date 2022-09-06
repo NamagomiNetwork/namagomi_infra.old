b42f5e7d8c98e069fa14581fc4b3fd812f1af60d
nabr <100725601+nabr2730@users.noreply.github.com>
2022-09-06 14:41:29 +0900

    update start.sh

--- Minecraft-Server/Namagomi-mod/start.sh
+++ Minecraft-Server/Namagomi-mod/start.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+readonly SERVERDIR=`dirname $0`
 
-cd /home/mc-server/mc || exit
-# 各種configはgithubから引っ張る
-rm ops.json server.properties whitelist.json
-wget -q --show-progress --https-only --timestamping \
-  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/ops.json \
-  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/server.properties \
-  https://raw.githubusercontent.com/NamagomiNetwork/namagomi_infra/main/Minecraft-Server/Namagomi-mod/configs/whitelist.json
+cd ${SERVERDIR}
 
-screen -UAmdS mc java -server -Xmx10G -Duser.timezone=JST -jar server.jar
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java \
+  -Dfile.encording=UTF-8 \
+  -verbose:gc \
+  -server \
+  -Xms28G \
+  -Xmx28G \
+  -XX:MetaspaceSize=512M \
+  -XX:+UseG1GC \
+  -XX:+UseStringDeduplication \
+  -jar server.jar \
+  nogui
